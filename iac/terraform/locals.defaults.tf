59d0702aa4c968dbe71d524401aa33405029fb01
Timo Surmann <timo.surmann@rwe.com>
2022-12-08 14:22:36 +0100

    debug

--- iac/terraform/locals.defaults.tf
+++ iac/terraform/locals.defaults.tf
@@ -7,6 +7,6 @@ locals {
   _secrets = {
     bing_maps_key                             = "Ar6iuHZYgX1BrfJs6SRJaXWbpU_HKdoe7G-OO9b2kl3rWvcawYx235GGx5FPM76O"
     mssql_server_administrator_login          = "demousersa"
-    mssql_server_administrator_login_password = "demo!P@55w0rd123"
+    mssql_server_administrator_login_password = "demo!P@55w0rd1234"
   }
 }
